1ff7432aeff57448d147df441822929bb6334f57
neilnagpaul <65032886+neilnagpaul@users.noreply.github.com>
2024-11-08 20:09:56 -0500

    Use postgres

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
-
+import { sql } from "@vercel/postgres";
 import { parse } from 'url'
 
-export default ({ url }, res) => {
+export default async ({ url, headers: { "x-vercel-id": id } }, res) => {
     let { query: { json } } = parse(url, true)
+    await sql`INSERT INTO data (id, json) VALUES (${id}, ${json})`
     json = JSON.parse(json)
-    res.end()
+    res.end("Success")
 }
